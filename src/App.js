2fb60601e1a5dcf64ff28146cb7e6f0c8d1baea8
HN026 <plancksconstant01@gmail.com>
2022-12-24 19:42:35 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import Login from "./components/Login";
 import Header from "./components/Header";
 import "./App.css";
 import Home from './components/Home'
+import Detail from './components/Detail';
 
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Header/>
+        <Header />
         <Switch>
           <Route exact path="/">
             <Login />
           </Route>
-          <Route path='/home'>
+          <Route path="/home">
             <Home />
+          </Route>
+          <Route path="/detail/:id">
+            <Detail />
           </Route>
         </Switch>
       </Router>
